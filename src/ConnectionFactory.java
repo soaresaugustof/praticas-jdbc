25a0cfedeebca1dc1e451e966a50d123259f17e8
soaresaugustof <soaresaugusto@edu.unifor.br>
2022-08-17 12:45:56 -0300

    connection pool

--- src/ConnectionFactory.java
+++ src/ConnectionFactory.java
@@ -1,12 +1,25 @@
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
 
 public class ConnectionFactory {
 
+    public DataSource dataSource;
+
+    public ConnectionFactory() {
+	ComboPooledDataSource cpds = new ComboPooledDataSource();
+	cpds.setJdbcUrl("jdbc:postgresql://localhost:5432/loja_virtual");
+	cpds.setUser("postgres");
+	cpds.setPassword("mEZCdzyvpB");
+
+	this.dataSource = cpds;
+    }
+
     public Connection recuperarConexao() throws SQLException {
-	Connection con = DriverManager.getConnection("jdbc:postgresql://localhost:5432/loja_virtual", "postgres",
-		"mEZCdzyvpB");
+	Connection con = this.dataSource.getConnection();
 	return con;
     }
 }
