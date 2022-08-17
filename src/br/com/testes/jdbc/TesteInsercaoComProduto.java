683b539b9f53f2c996b2edad59fe5a0e4908af5e
soaresaugustof <soaresaugusto@edu.unifor.br>
2022-08-17 13:40:28 -0300

    dao

--- src/br/com/testes/jdbc/TesteInsercaoComProduto.java
+++ src/br/com/testes/jdbc/TesteInsercaoComProduto.java
@@ -1,11 +1,9 @@
 package br.com.testes.jdbc;
 
 import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 
+import br.com.testes.jdbc.dao.ProdutoDAO;
 import br.com.testes.jdbc.modelo.Produto;
 
 public class TesteInsercaoComProduto {
@@ -15,21 +13,9 @@ public class TesteInsercaoComProduto {
 	Produto produto = new Produto("CÔMODA", "CÔMODA VERTICAL");
 
 	try (Connection connection = new ConnectionFactory().recuperarConexao()) {
-
-	    String sql = "INSERT INTO PRODUTO (NOME, DESCRICAO) VALUES (?, ?)";
-
-	    try (PreparedStatement pstm = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
-		pstm.setString(1, produto.getNome());
-		pstm.setString(2, produto.getDescricao());
-
-		pstm.execute();
-
-		try (ResultSet rst = pstm.getGeneratedKeys()) {
-		    while (rst.next()) {
-			produto.setId(rst.getInt(1));
-		    }
-		}
-	    }
+	    ProdutoDAO produtoDAO = new ProdutoDAO(connection);
+	    produtoDAO.salvar(produto);
+	    // produtoDAO.listar();
 	}
 	System.out.println(produto);
     }
